2f91049143327d06e175ffd4bb95bae225eff9ea
capidani <danielcapitanlopez@MacBook-Pro-de-Daniel-2.local>
2017-07-21 14:56:30 +0200

    v1

---
+++ aplicacion_TFG/prediccion.py
@@ -0,0 +1,101 @@
+'''
+Author          :Adil Moujahid
+'''
+
+import os
+import glob
+import cv2
+import caffe
+import lmdb
+import numpy as np
+from caffe.proto import caffe_pb2
+
+caffe.set_mode_gpu() 
+
+#Size of images
+IMAGE_WIDTH = 227
+IMAGE_HEIGHT = 227
+
+'''
+Image processing helper function
+'''
+
+def transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT):
+
+    #Histogram Equalization
+    img[:, :, 0] = cv2.equalizeHist(img[:, :, 0])
+    img[:, :, 1] = cv2.equalizeHist(img[:, :, 1])
+    img[:, :, 2] = cv2.equalizeHist(img[:, :, 2])
+
+    #Image Resizing
+    img = cv2.resize(img, (img_width, img_height), interpolation = cv2.INTER_CUBIC)
+
+    return img
+
+
+'''
+Reading mean image, caffe model and its weights 
+'''
+#Read mean image
+mean_blob = caffe_pb2.BlobProto()
+with open('/home/user_cudnn/pruebanet/mean.binaryproto') as f:
+    mean_blob.ParseFromString(f.read())
+mean_array = np.asarray(mean_blob.data, dtype=np.float32).reshape(
+    (mean_blob.channels, mean_blob.height, mean_blob.width))
+
+
+#Read model architecture and trained model's weights
+net = caffe.Net('/home/user_cudnn/pruebanet/pruebanet_deploy_1.prototxt',
+                '/home/user_cudnn/pruebanet/caffe_model_1_iter_10000.caffemodel',
+                caffe.TEST)
+
+#Define image transformers
+transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+transformer.set_mean('data', mean_array)
+transformer.set_transpose('data', (2,0,1))
+
+'''
+Making predicitions
+'''
+#Reading image paths
+test_img_paths = [img_path for img_path in glob.glob("test1/*jpg")]
+
+#Making predictions
+test_ids = []
+preds = []
+for img_path in test_img_paths:
+    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    img = transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT)
+    
+    net.blobs['data'].data[...] = transformer.preprocess('data', img)
+    out = net.forward()
+    pred_probas = out['prob']
+
+    test_ids = test_ids + [img_path.split('/')[-1][:-4]]
+    preds = preds + [pred_probas.argmax()]
+
+
+
+    print img_path
+    print pred_probas
+    print pred_probas.argmax()
+    if pred_probas.argmax()==1:
+        print 'ES UN PERRO'
+    else:
+        print 'ES UN GATO'
+    print '-------'
+
+'''
+Making submission file
+'''
+with open("caffe_model_1/submission_model_1.csv","w") as f:
+    f.write("id,tipo,label\n")
+    for i in range(len(test_ids)):
+        if preds[i]==1:
+            tipo = 'PERRO'
+        else:
+            tipo ='GATO'
+    
+
+        f.write(str(test_ids[i])+","+str(tipo)+","+str(preds[i])+"\n")
+f.close()
